147a1bdcd552adf4e715fc7ebe60988f0cee1778
Mukesh <brainstorm4x@gmail.com>
2022-08-09 03:49:21 +0530

    Added the Show,edit and delete links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,41 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article 
+    else 
+      render 'new'
+    end
+    # redirect_to article_path(@article) this is so common that we use the above line instead
+    #rails will extract the id from @acticles as we ar diret it to show article paage which needs an id
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
 end
