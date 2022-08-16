de6d3a1f68bccf14910ed56e7e7873298421eadd
Mukesh <brainstorm4x@gmail.com>
2022-08-16 09:54:28 +0530

    Added restrictions and modified navigation links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
-    @article = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up!"
       redirect_to articles_path
     else 
@@ -24,10 +28,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(params.require(:user).permit(:username, :email, :password))
       flash[:notice] = "Your profile was successfully updated!"
-      redirect_to articles_path
+      redirect_to @user 
     else
       render 'edit'
     end
@@ -37,6 +40,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
